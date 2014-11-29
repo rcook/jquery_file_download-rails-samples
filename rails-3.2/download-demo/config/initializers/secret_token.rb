4991db00475067993e4a9a90568de259be65a998
Richard Cook <rcook@rcook.org>
2014-11-29 12:47:43 -0800

    (1) Create app

---
+++ rails-3.2/download-demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DownloadDemo::Application.config.secret_token = 'b0542d6fd7f3618629dac53c2e0fd766e7a025666a4ddf056aac0077e658d144de65e17f4612de37bd1f0feff425477e6f5b6f3c60d2b28b01b964c36b792186'
