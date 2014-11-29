e46bedc1cb77e26306502893404f27e4e4ee21d3
Richard Cook <rcook@rcook.org>
2014-11-29 12:54:26 -0800

    (6) Add page and file actions to controller

--- rails-3.2/download-demo/app/controllers/downloads_controller.rb
+++ rails-3.2/download-demo/app/controllers/downloads_controller.rb
@@ -1,2 +1,14 @@
 class DownloadsController < ApplicationController
+  def page
+  end
+
+  def file
+    cookies['fileDownload'] = 'true'
+
+    send_file File.join(Rails.root, 'file.txt'),
+      filename: 'downloaded-file.txt',
+      type: 'content-type',
+      x_sendfile: true
+  end
 end
+
